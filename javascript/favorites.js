e0f221fd82085731f3824303c9ad59afe760ce09
Luiz Fernando Pinto de Souza Verly do Nascimento <luizfernando.cg@hotmail.com>
2022-06-06 19:54:41 -0300

    project javascript and css finished

---
+++ javascript/favorites.js
@@ -0,0 +1,130 @@
+import { GithubUser } from './githubUser.js'
+
+class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root)
+    this.load()
+  }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem('@git-fav:')) || []
+  }
+
+  save() {
+    localStorage.setItem('@git-fav:', JSON.stringify(this.entries))
+  }
+
+  async add(username) {
+    try {
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      if (userExists) {
+        throw new Error('User already exists')
+      }
+
+      const user = await GithubUser.search(username)
+
+      if (user.login === undefined) {
+        throw new Error("This user don't exist")
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(
+      entry => entry.login !== user.login
+    )
+    this.entries = filteredEntries
+    this.update()
+    this.save()
+  }
+}
+
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root)
+
+    this.tbody = this.root.querySelector('.content-box table tbody')
+
+    this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.input-search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.input-search input')
+
+      this.add(value)
+    }
+  }
+
+  NoEntries() {
+    this.entries.length === 0
+      ? document.querySelector('.without-entries').classList.remove('sr-only')
+      : document.querySelector('.without-entries').classList.add('sr-only')
+  }
+
+  update() {
+    this.NoEntries()
+
+    this.removeAllTr()
+
+    this.entries.forEach(user => {
+      const row = this.createRow()
+
+      row.querySelector(
+        '.user img'
+      ).src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Image of ${user.name}`
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
+      row.querySelector('.user p').textContent = `${user.name}`
+      row.querySelector('.user span').textContent = `${user.login}`
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Are you sure you want to delete this user?')
+        if (isOk) {
+          this.delete(user)
+        }
+      }
+
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `
+    <td class="user">
+    <img
+    src="https://github.com/Luizfpsvn.png"
+    alt="Luiz Fernando image"
+    />
+    <a href="https://github.com/Luizfpsvn" target="_blank">
+    <p>Luiz Fernando</p>
+    <span>/Luizfpsvn</span>
+        </a>
+        </td>
+        <td class="repositories">76</td>
+        <td class="followers">9589</td>
+        <td class="remove">
+        <button class="remove">Remove</button>
+        </td>
+        `
+
+    return tr
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll('tr').forEach(tr => {
+      tr.remove()
+    })
+  }
+}
